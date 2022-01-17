cdd291fbc09ed6811d9cd4f54833dc3a0e46a5d2
luizsmatos <gusstavopdm@gmail.com>
2022-01-17 12:46:09 -0300

    feature: É possivel listar as Todos

--- src/index.js
+++ src/index.js
@@ -11,19 +11,28 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+  return next();
 }
+
 
 app.post("/users", (request, response) => {
   const { name, username } = request.body;
-  const findUsers = users.find((user) => user.username === username);
+  const user = users.find((user) => user.username === username);
 
-  if (findUsers) {
-    response.status(400).json({ error: "User already exists." });
+  if (user) {
+    return response.status(400).json({ error: "User already exists." });
   }
 
   if (!name || !username) {
-    response.status(400).json({ error: "Missing name or username" });
+    return response.status(400).json({ error: "Missing name or username" });
   }
 
   const newUser = {
@@ -34,27 +43,25 @@ app.post("/users", (request, response) => {
   };
   users.push(newUser);
 
-  response.status(201).json(newUser);
+  return response.status(201).json(newUser);
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
 });
 
-app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put("/todos/:id", checksExistsUserAccount, checksExistsTodo, (request, response) => {
 });
 
-app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch("/todos/:id/done", checksExistsUserAccount, checksExistsTodo, (request, response) => {
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
 });
 
 module.exports = app;
